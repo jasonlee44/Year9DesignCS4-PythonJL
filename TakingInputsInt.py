c6aab3691a7ab4f718df447aa7c17b25c30f8d0f
Jason Lee <jason.c.lee@Jason-Lee22.local>
2018-09-12 13:59:05 -0400

    Surface area of cylinder

--- TakingInputsInt.py
+++ TakingInputsInt.py
@@ -1,7 +1,12 @@
 #Input
 #Assignment Statement
 r = input("What is the radius: ")
-h = input("What is the height: ")
+r = int(r) #changes the input into an integer 
+
+h = int(input("What is the height: "))
+
 #Process
+ans = 2*3.14*r*r + 2*r*h*3.14
 
 #Output
+printLine(ans)
