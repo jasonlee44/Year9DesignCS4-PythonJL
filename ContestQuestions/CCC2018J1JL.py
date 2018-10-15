ebf4c080455ccf039696ca14e01575796a003e46
Jason Lee <jason.c.lee@Jason-Lee22.local>
2018-10-15 12:10:42 -0400

    ContestQuestions updated

--- ContestQuestions/CCC2018J1JL.py
+++ ContestQuestions/CCC2018J1JL.py
@@ -1,19 +1,17 @@
 #Input
 a = int(input(""))
-
 b = int(input(""))
-
-
 c = int(input(""))
-
-
 d = int(input(""))
 
-
-
 #Process
+if a == 8 or a == 9:
+	if d == 8 or d == 9:
+		if b == c:
+#Output			
+			print("ignore")
+else:
+	print("answer")
 
 
 
-
-#Output
